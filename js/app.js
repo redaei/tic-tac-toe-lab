948b4ab8983ecde141a626257941fe4820e564ea
redaei <redaeis@gmail.com>
2024-09-26 14:09:13 +0300

    step 6a

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const winningCombos = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+]
 /*---------------------------- Variables (state) ----------------------------*/
 let board = ['', '', '', '', '', '', '', '', '']
 let turn = 'X'
@@ -8,6 +17,8 @@ let tie = false
 
 /*------------------------ Cached Element References ------------------------*/
 const squareEls = document.querySelectorAll('.sqr')
+const message = document.getElementById("message")
+
 /*-------------------------------- Functions --------------------------------*/
 const init = () => {
   render()
@@ -20,12 +31,32 @@ const render = () => {
 
 const updateBoard = () => {
   board.forEach((sqr, index) => {
-    squareEls[index].textContent = turn
+    squareEls[index].textContent = board[index]
   })
 }
+
 const updateMessage = () => {
   // -If both winner and tie have a value of false (meaning the game is still in progress), render whose turn it is.
+  if (!winner && !tie){
+    message.innerText = turn
+  }
+  else if (winner === false && tie === true){
+    message.innerText = "Tie!"
+  }
+  else {
+    message.innerText = `Bravo! · ${turn} win!`
+  }
   // -If winner is false, but tie is true, render a tie message.
   // -Otherwise, render a congratulatory message to the player that has won.
 }
+
+const handleClick = (event) => {
+  //test
+  console.log(event);
+  
+}
 /*----------------------------- Event Listeners -----------------------------*/
+
+squareEls.forEach((addEventListener('click', (event)=>{
+  handleClick(event)
+})))
